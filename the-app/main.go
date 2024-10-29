d50e2b6187dfb00f842b7c1215197a81b5ad37d0
Roaan Vos <rvos@atlassian.com>
2024-10-29 21:09:03 +1100

    Return route info

--- the-app/main.go
+++ the-app/main.go
@@ -1,30 +1,46 @@
 package main
+
 import (
-    "net/http"
-    _ "embed"
-    "github.com/gin-gonic/gin"
-	"runtime"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"runtime"
 )
 
 // album represents data about a record album.
 func main() {
-    router := gin.Default()
-    router.GET("/ping", ping)
-    router.GET("/arch", arch)
-    router.GET("/healthcheck", healthcheck)
+	router := gin.Default()
+	router.GET("/", home(router))
+	router.GET("/ping", ping)
+	router.GET("/arch", arch)
+	router.GET("/healthcheck", healthcheck)
 
-    router.Run("0.0.0.0:8080")
+	router.Run("0.0.0.0:8080")
 }
 
 func ping(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, gin.H{
+	c.IndentedJSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
 }
 
+func home(router *gin.Engine) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		var routeInfo []string
+		for _, aRoute := range router.Routes() {
+			routeInfo = append(routeInfo, fmt.Sprintf("%s %s", aRoute.Method, aRoute.Path)) // note the = instead of :=
+		}
+
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"routes": routeInfo,
+		})
+	}
+
+	return gin.HandlerFunc(fn)
+}
+
 func arch(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, gin.H{
+	c.IndentedJSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	})
 }
@@ -33,4 +49,3 @@ func healthcheck(c *gin.Context) {
 	//c.Data(http.StatusOK, "application/text", []byte("OK"))
 	c.String(http.StatusOK, "OK")
 }
-
