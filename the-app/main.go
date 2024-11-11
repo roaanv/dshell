36b72b10c8f2a38914543818f0b8ac2aa4b27ad2
Roaan Vos <rvos@atlassian.com>
2024-11-11 20:54:16 +1100

    Added probe endpoints

--- the-app/main.go
+++ the-app/main.go
@@ -3,9 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 	"runtime"
 )
+
+func init() {
+    lvl, ok := os.LookupEnv("LOG_LEVEL")
+    // LOG_LEVEL not set, let's default to debug
+    if !ok {
+        lvl = "debug"
+    }
+    // parse string, this is built-in feature of logrus
+    ll, err := log.ParseLevel(lvl)
+    if err != nil {
+        ll = log.DebugLevel
+    }
+    // set global log level
+    log.SetLevel(ll)
+}
+
 
 // album represents data about a record album.
 func main() {
@@ -14,11 +32,15 @@ func main() {
 	router.GET("/ping", ping)
 	router.GET("/arch", arch)
 	router.GET("/healthcheck", healthcheck)
+	router.GET("/probe/live", liveProbe)
+	router.GET("/probe/ready", readyProbe)
+	router.GET("/probe/startup", startupProbe)
 
 	router.Run("0.0.0.0:8080")
 }
 
 func ping(c *gin.Context) {
+	log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
@@ -26,6 +48,7 @@ func ping(c *gin.Context) {
 
 func home(router *gin.Engine) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
+		log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
 		var routeInfo []string
 		for _, aRoute := range router.Routes() {
 			routeInfo = append(routeInfo, fmt.Sprintf("%s %s", aRoute.Method, aRoute.Path)) // note the = instead of :=
@@ -40,12 +63,28 @@ func home(router *gin.Engine) gin.HandlerFunc {
 }
 
 func arch(c *gin.Context) {
+	log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	})
 }
 
 func healthcheck(c *gin.Context) {
-	//c.Data(http.StatusOK, "application/text", []byte("OK"))
+	log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
+	c.String(http.StatusOK, "OK")
+}
+
+func liveProbe(c *gin.Context) {
+	log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
+	c.String(http.StatusOK, "OK")
+}
+
+func readyProbe(c *gin.Context) {
+	log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
+	c.String(http.StatusOK, "OK")
+}
+
+func startupProbe(c *gin.Context) {
+	log.Debugln(fmt.Sprintf("called '/%s'", c.Request.URL.Path))
 	c.String(http.StatusOK, "OK")
 }
